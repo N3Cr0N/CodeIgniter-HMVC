94701da08703373bff314e2007e03cde38db6d99
N3Cr0N <n3cr0n@list.ru>
2019-02-03 01:43:12 +0100

    Initial commit

--- application/modules/welcome/controllers/Welcome.php
+++ application/modules/welcome/controllers/Welcome.php
@@ -20,6 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('views/welcome_message');
+		$this->load->view('welcome_message');
 	}
 }
--- application/modules/welcome/views/welcome_message.php
+++ application/modules/welcome/views/welcome_message.php
@@ -74,10 +74,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>application/modules/welcome/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+		<code>application/modules/welcome/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
