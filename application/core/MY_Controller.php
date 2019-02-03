4ef216bb596f8c79a6da9f20acc53f62d8f07b9a
N3Cr0N <n3cr0n@list.ru>
2019-02-03 01:29:00 +0100

    Initial commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php
+(defined('BASEPATH')) or exit('No direct script access allowed');
+
+class MY_Controller extends MX_Controller
+{
+    /**
+     * [__construct description]
+     *
+     * @method __construct
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
