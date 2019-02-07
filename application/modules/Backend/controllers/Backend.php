b761fef892fb145a34c66785bdafcc4f8e2b9d48
N3Cr0N <n3cr0n@list.ru>
2019-02-07 21:22:19 +0100

    Added backend and frontend controllers for HMVC

---
+++ application/modules/Backend/controllers/Backend.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * CodeIgniter-HMVC
+ *
+ * @package    CodeIgniter-HMVC
+ * @author     N3Cr0N (N3Cr0N@list.ru)
+ * @copyright  2019 N3Cr0N
+ * @license    https://opensource.org/licenses/MIT  MIT License
+ * @link       <URI> (description)
+ * @version    GIT: $Id$
+ * @since      Version 0.0.1
+ * @filesource
+ * @todo       (description)
+ *
+ */
+
+class Backend extends BackendController
+{
+    /**
+     * [__construct description]
+     *
+     * @method __construct
+     */
+    public function __construct()
+    {
+        //
+        parent::__construct();
+    }
+
+    /**
+     * [index description]
+     *
+     * @method index
+     *
+     * @return [type] [description]
+     */
+    public function index()
+    {
+        // Example
+        //$this->load->view('backend/dashboard');
+    }
+}
