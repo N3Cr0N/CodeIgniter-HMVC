0f9dab16680afd76a7a368ec07c8a23dcb4404fa
N3Cr0N <n3cr0n@list.ru>
2019-02-07 22:32:10 +0100

    Added protected $data, public $CI and constructor

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -18,6 +18,30 @@
 /* load the MX_Router class */
 require APPPATH . "third_party/MX/Router.php";
 
+
 class MY_Router extends MX_Router
 {
+    //
+    public $CI;
+
+    /**
+     * An array of variables to be passed through to the
+     * view, layout,....
+     */
+    protected $data = array();
+
+    /**
+     * [__construct description]
+     *
+     * @method __construct
+     */
+    public function __construct()
+    {
+        // To inherit directly the attributes of the parent class.
+        parent::__construct();
+
+        // This function returns the main CodeIgniter object.
+        // Normally, to call any of the available CodeIgniter object or pre defined library classes then you need to declare.
+        $CI =& get_instance();
+    }
 }
