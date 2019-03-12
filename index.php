4e60548f5b9d856d039181c8952e1a68f3d0dcab
N3Cr0N <ByteKingz@mail.ru>
2019-03-12 22:16:06 +0100

    Delete php error reporting for production environment, added php error reporting for testing environment

--- index.php
+++ index.php
@@ -98,15 +98,16 @@ switch (ENVIRONMENT) {
     break;
 
     case 'testing':
-    break;
-
-    case 'production':
-        ini_set('display_errors', 0);
+        ini_set('display_errors', 1);
         if (version_compare(PHP_VERSION, '5.3', '>=')) {
             error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
         } else {
             error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
         }
+    break;
+
+    case 'production':
+        ini_set('display_errors', 0);
     break;
 
     default:
